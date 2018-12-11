f7a28d63bb4167620488c9d66b5067ae0f9931a9
Menachem Aber <abermendel@gmail.com>
2018-12-11 22:02:24 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  alphabet_hash = {}
+  ESPERANTO_ALPHABET.split("").each_with_index do |letter, index|
+    alphabet_hash[letter] = index
+  end
+  
+  arr.sort_by do |i|
+    alphabet_hash[i[0]]
+    # binding.pry
+  end
 end
